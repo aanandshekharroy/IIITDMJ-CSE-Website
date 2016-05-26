1dbb33644276846c7812719094731de4d3b29950
theseus007 <2013001@iiitdmj.ac.in>
2016-05-26 22:45:11 +0530

    idk

--- source/resources/views/welcome.blade.php
+++ source/resources/views/welcome.blade.php
@@ -945,160 +945,10 @@
             <p class="lead">Lorem ipsum dolor sit amet, ius minim gubergren ad.</p>
             </div>
         </div>-->
-        
-        <div class="row">
-            <!--<div class="col-md-6">
-       			<div class="media">
-                   <div class="circ-wrapper pull-left"><h3>25<br>June</h3></div>
-                      <div class="media-body">
-                      	<h4 class="media-heading"><a href="javascript:void(0)">CSS3 Editable Flat effect</a> <span class="label label-warning">NEW</span></h4>
-                         <p>Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex.</p>
-                         <ul class="data-lessons">
-                              <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-clock"></i>Duration: 6 hours</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Duration: 6 hours</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body">
-                                            <p class="no_margin">Lorem ipsum dolor sit amet, has at illum dictas definitiones, ei primis indoctum torquatos nec. Vis te velit probatus, natum atomorum tincidunt nec an.</p>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                                    <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-video"></i>Video files</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Video files</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body"><ul class="list_po_body"><li><i class="icon-ok"></i> 2 Video lessons</li><li> <i class="icon-ok"></i>1 Video for practice</li><li> <i class="icon-ok"></i>1 Video Quiz</li></ul>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                                    <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-mic"></i>Audio files</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Audio files</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body">
-                                            <p class="no_margin">Lorem ipsum dolor sit amet, has at illum dictas definitiones, ei primis indoctum torquatos nec. Vis te velit probatus, natum atomorum tincidunt nec an.</p>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                          </ul>
-                      </div>
-                    </div>
-                    
-                    <div class="media">
-                   <div class="circ-wrapper pull-left"><h3>15<br>July</h3></div>
-                      <div class="media-body">
-                        <h4 class="media-heading"><a href="javascript:void(0)">CSS3 Workshop</a> <span class="label label-warning">NEW</span></h4>
-                         <p>Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex.</p> 
-                         <ul class="data-lessons">
-                              <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-clock"></i>Duration: 6 hours</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Duration: 6 hours</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body">
-                                            <p class="no_margin">Lorem ipsum dolor sit amet, has at illum dictas definitiones, ei primis indoctum torquatos nec. Vis te velit probatus, natum atomorum tincidunt nec an.</p>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                                    <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-video"></i>Video files</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Video files</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body"><ul class="list_po_body"><li><i class="icon-ok"></i> 2 Video lessons</li><li> <i class="icon-ok"></i>1 Video for practice</li><li> <i class="icon-ok"></i>1 Video Quiz</li></ul>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                                    <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-doc-text-inv"></i>Text doc</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Text doc</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body">
-                                            <p class="no_margin">Lorem ipsum dolor sit amet, has at illum dictas definitiones, ei primis indoctum torquatos nec. Vis te velit probatus, natum atomorum tincidunt nec an.</p>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                          </ul>
-                      </div>
-                    </div>
-                    
-            </div><!-- End col md 6 -->
-            <!--<div class="col-md-6">
-            <div class="media">
-                   <div class="circ-wrapper pull-left"><h3>12<br>Aug</h3></div>
-                      <div class="media-body">
-                      	<h4 class="media-heading"><a href="javascript:void(0)">CSS3 Editable Flat effect</a></h4>
-                         <p>Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex.</p>
-                         <ul class="data-lessons">
-                              <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-clock"></i>Duration: 6 hours</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Duration: 6 hours</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body">
-                                            <p class="no_margin">Lorem ipsum dolor sit amet, has at illum dictas definitiones, ei primis indoctum torquatos nec. Vis te velit probatus, natum atomorum tincidunt nec an.</p>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                                    <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-video"></i>Video files</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Video files</strong></div> <!-- ./po-title -->
-                                       <!-- <div class="po-body"><ul class="list_po_body"><li><i class="icon-ok"></i> 2 Video lessons</li><li> <i class="icon-ok"></i>1 Video for practice</li><li> <i class="icon-ok"></i>1 Video Quiz</li></ul>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                                    <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-mic"></i>Audio files</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Audio files</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body">
-                                            <p class="no_margin">Lorem ipsum dolor sit amet, has at illum dictas definitiones, ei primis indoctum torquatos nec. Vis te velit probatus, natum atomorum tincidunt nec an.</p>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                          </ul>
-                      </div>
-                    </div>
-                    
-                    <div class="media">
-                   <div class="circ-wrapper pull-left"><h3>25<br>Sept</h3></div>
-                      <div class="media-body" >
-                        <h4 class="media-heading"><a href="javascript:void(0)">CSS3 Workshop</a></h4>
-                         <p>Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset, doctus volumus explicari qui ex.</p>
-                         <ul class="data-lessons">
-                              <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-clock"></i>Duration: 6 hours</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Duration: 6 hours</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body">
-                                            <p class="no_margin">Lorem ipsum dolor sit amet, has at illum dictas definitiones, ei primis indoctum torquatos nec. Vis te velit probatus, natum atomorum tincidunt nec an.</p>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                                    <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-video"></i>Video files</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Video files</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body"><ul class="list_po_body"><li><i class="icon-ok"></i> 2 Video lessons</li><li> <i class="icon-ok"></i>1 Video for practice</li><li> <i class="icon-ok"></i>1 Video Quiz</li></ul>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                                    <li class="po-markup">
-                                    <a class="po-link" href="javascript:void(0)" ><i class="icon-doc-text-inv"></i>Text doc</a>
-                                    <div class="po-content hidden">
-                                        <div class="po-title"><strong>Text doc</strong></div> <!-- ./po-title -->
-                                        <!--<div class="po-body">
-                                            <p class="no_margin">Lorem ipsum dolor sit amet, has at illum dictas definitiones, ei primis indoctum torquatos nec. Vis te velit probatus, natum atomorum tincidunt nec an.</p>
-                                        </div><!-- ./po-body -->
-                                    <!--</div><!-- ./po-content -->
-                                    <!--</li>
-                          </ul> 
-                      </div>
-                    </div>
-                
-            </div>
-        --></div><!-- End row -->
+    
 		<hr>
 		<div class="row">
-		<div class="span12" >
+		  <div class="span12" >
 			<div class="card col-md-5 card-style m-15" style="height:526.02px;overflow:hidden;">
 				<header class="nav-style2">
 					<div class="container-fluid">
@@ -1134,7 +984,8 @@
                     </div>
                     @endforeach
                 @endif
-				
+				</div>
+            </div>
 			<div class="card col-md-4 card-style m-15" >
 				<header class="nav-style3">
 					<div class="container-fluid">
